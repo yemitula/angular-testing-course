dbf565d007cc230ca5a5d53fb8cff35acf6c8ce0
Yemi Tula <yemi@tulabyte.net>
2021-07-02 18:18:19 -0400

    Create courses.service.spec.ts

---
+++ src/app/courses/services/courses.service.spec.ts
@@ -0,0 +1,24 @@
+import { TestBed } from "@angular/core/testing";
+import { CoursesService } from "./courses.service";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+describe("CoursesService", () => {
+
+    let coursesService: CoursesService,
+        httpTestingController: HttpTestingController;
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [
+                CoursesService
+            ]
+        });
+
+        coursesService = TestBed.get(CoursesService);
+        httpTestingController = TestBed.get(HttpTestingController);
+    });
+
+    it("should retrieve all courses", () => {
+
+    });
+})
